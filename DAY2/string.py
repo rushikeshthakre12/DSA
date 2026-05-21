d9dacd1b12dc2536824a636026c4f81bb8b66aa5
Rushikesh Thakre <rushikeshthakre12@gmail.com>
2026-05-21 23:03:57 +0530

    Update string.py

--- DAY2/string.py
+++ DAY2/string.py
@@ -149,49 +149,6 @@
 # print("Prashant jha".count("a"))
 
 #----------------------------------------------------
-#Pattern Coding
-#1
-# for i in range(1,4):
-#     for j in range(1,4):
-#         print(i,end=" ")
-#     print()
-
-#2
-# n=int(input("Enter the number of rows: "))
-# for i in range(1,n+1):
-#     for j in range(1,n+1):
-#         print(chr(64+i),end=" ")
-#     print()
-
-#3
-# n=int(input("Enter the number of rows: "))
-# for i in range(1,n+1):
-#     for j in range(1,1+i):
-#         print("*",end=" ")
-#     print()
-
-#4
-# n=int(input("Enter the number of rows: "))
-# for i in range(1,n+1):
-#     for j in range(1,n+2-i):
-#         print(chr(64+j),end=" ")
-#     print()
-
-#5
-# import time
-
-# n=int(input("Enter the number of rows: "))
-# for i in range(1,n+1):
-#     print(" "*(n-i),end=" ")
-#     for j in range(1,1+i):
-#         time.sleep(3)
-#         print("*",end=" ")
-#     print()
-
-#i/p-1,2,3,4
-#o/p-24,12,8,6
-
-    
 
 # i/p- "Hello world"
 #o/p- "olleH dlrow"
@@ -202,6 +159,4 @@
 #     print(i[::-1], end=" ")
 
 
-#i/p- ({[()]})
-#o/p- Valid
 
