64744432a68afdd2b22e1add90f029ea39d34863
Rushikesh Thakre <rushikeshthakre12@gmail.com>
2026-05-22 19:53:24 +0530

    Update list1.py

--- Day_3/list/list1.py
+++ Day_3/list/list1.py
@@ -1,4 +1,56 @@
-arr = [[1,2,3],22,[4,5]]
-print(arr)
-for i in range(len(arr)):
-    print(arr[i])
+# print(mylist)
+# print(type(mylist))#<List>
+# print(mylist[0]) #prashant
+# print(mylist[1]) #Ashish
+# print(mylist[2]) #komal
+# print(mylist[-1]) #prashant last one
+# print(mylist[2:5]) #n=5,n-1=4
+# print(mylist[:5]) #n=5, n-1=4
+# print(mylist[1:]) #n=8,n-1=8-1=7
+# print(mylist[1:8:2])#1,3,5,7...
+
+# mylist[2]="Akshay" #list is mutable(changable)
+# print(mylist)
+
+# if "ankush" in mylist:
+#     print("yes ankush is avilable")
+# else:
+#     print("not avilable")
+
+# mylist.append("harsh") #append function is used in list and it added in right side
+# mylist.append("laxman")
+# print(mylist)
+# #apppend() and extend() btoh work like same
+
+# mylist.insert(3,"sanket")
+# print(mylist)
+
+# mylist.remove("sandip")
+# print(mylist)
+
+# newlist = mylist.copy() #cloning
+# print(newlist)
+
+# mylist = [["prashant","jha"],[85.56],[440022,"yyy"]]
+# print("example of multidimensional list: ")
+# print(mylist)
+# #print(mylist[row][column])
+# print(mylist[0][0])#prashant
+# print(mylist[0][1])#jha
+# print(mylist[1][0])#85.56
+# print(mylist[2][0])#440022
+# print(mylist[2][1])#yyy
+# #     0          1
+# #0= [["prashant","jha"],
+# #1= [85.56],
+# #2= [440022,"yyy"]]
+
+
+# list2 =[50,25.50,'prashant']
+# del list2[2] #for delete particular element 
+# #del list2 #for deleting entire list
+# print(list2)
+
+# list2 =[50,25.50,'prashant']
+# list2.clear()
+# print(list2)
