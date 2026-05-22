57e69f88699e5e2c41fac9d6abda7ebff1c378f0
Rushikesh Thakre <rushikeshthakre12@gmail.com>
2026-05-22 19:43:52 +0530

    Delete DSA directory

--- DSA/Day1/Conditions/EvenOdd.py
+++
@@ -1,5 +0,0 @@
-num=5
-if num%2==0:
-    print(num,"is an even number")
-else:   
-     print(num,"is an odd number")
--- DSA/Day1/loop/forloop.py
+++
@@ -1,7 +0,0 @@
-#for i in range(10):
-    #print(i)
-#for i in range(1, 11):
-    #print(i)
-
-for i in range(10, 0, -1):
-    print(i) 
